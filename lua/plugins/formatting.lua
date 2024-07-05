ef50a7237991d0961020c270031b42223637b609
Ismail Rohaga <azh.zhafir@gmail.com>
2024-07-05 16:24:13 +0700

    fix: support all lang for prettier

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -1,4 +1,4 @@
----@diagnostic disable: inject-field, undefined-global
+---@diagnostic disable: inject-field, undefined-doc-name
 return {
   {
     "stevearc/conform.nvim",
@@ -6,14 +6,12 @@ return {
     ---@param opts ConformOpts
     opts = function(_, opts)
       opts.formatters_by_ft = opts.formatters_by_ft or {}
-      for _, ft in ipairs(supported) do
-        opts.formatters_by_ft[ft] = { "prettier" }
-      end
+      opts.formatters_by_ft["*"] = { "prettier" }
 
       opts.formatters = opts.formatters or {}
       opts.formatters.prettier = {
-        condition = function(_, ctx)
-          return M.has_parser(ctx) and (vim.g.lazyvim_prettier_needs_config ~= true or M.has_config(ctx))
+        condition = function(_, _)
+          return true
         end,
       }
     end,
