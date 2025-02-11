afb06c3f08195047729909463ae0a2601a61916c
Ismail Rohaga <azh.zhafir@gmail.com>
2025-02-11 09:28:00 +0700

    fix: import plugin bug and dotenv config

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -7,28 +7,26 @@ return {
   },
   {
     "neovim/nvim-lspconfig",
-    opts = function(_, opts)
-      local on_publish_diagnostics = vim.lsp.diagnostic.on_publish_diagnostics
-      opts.servers.bashls = vim.tbl_deep_extend("force", opts.servers.bashls or {}, {
-        handlers = {
-          ["textDocument/publishDiagnostics"] = function(err, res, ...)
-            local file_name = vim.fn.fnamemodify(vim.uri_to_fname(res.uri), ":t")
-            if string.match(file_name, "^%.env") == nil then
-              return on_publish_diagnostics(err, res, ...)
-            end
-          end,
+    opts = {
+      inlay_hints = { enabled = false }, -- Global inlay hints setting
+      servers = {
+        --  bashls specific configuration
+        bashls = {
+          handlers = {
+            ["textDocument/publishDiagnostics"] = function(err, res, ...)
+              local on_publish_diagnostics = vim.lsp.diagnostic.on_publish_diagnostics
+              local file_name = vim.fn.fnamemodify(vim.uri_to_fname(res.uri), ":t")
+              if string.match(file_name, "^%.env") == nil then
+                return on_publish_diagnostics(err, res, ...)
+              end
+            end,
+          },
         },
-      })
-    end,
+      },
+    },
   },
   {
     "mg979/vim-visual-multi",
     branch = "master", -- this is equivalent to the branch option in vim-plug
   },
-  { import = "lazyvim.plugins.extras.lang.typescript" },
-  { import = "lazyvim.plugins.extras.lang.json" },
-  { import = "lazyvim.plugins.extras.lang.rust" },
-  { import = "lazyvim.plugins.extras.linting.eslint" },
-  { import = "lazyvim.plugins.extras.formatting.prettier" },
-  { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
 }
